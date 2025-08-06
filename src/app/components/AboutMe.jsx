6e41bc30ea1f4b3fa779e105193dd3b483645643
Jacob <deatonja@oregonstate.edu>
2025-08-06 15:39:09 -0400

    Removed default content and created AboutMe, ContactMe, and WorkExperience components.

---
+++ src/app/components/AboutMe.jsx
@@ -0,0 +1,30 @@
+// About me section
+
+export default function AboutMe() {
+  return (
+    <section>
+      <h1>About Me</h1>
+        <p>
+            Hello! My name is Jacob Deaton, a software engineer passionate about leveraging technology and coding
+            to solve problems and create innovative solutions. I'm currently working towards a B.S. in Computer Science
+            at Oregon State University. Previously, I
+            had graduated from the University of Virginia with a B.A. in Economics. After working as a consultant for
+            two years, I realized that my true passion was with coding! This inspired me to return to school to
+            focus on building my skills in coding and software engineering.
+        </p>
+        <p>
+            Here are some technologies, languages, and frameworks that I have experience with:
+        </p>
+        <ul>
+            <li>Python</li>
+            <li>Swift</li>
+            <li>Supabase</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>SQL</li>
+            <li>Git</li>
+        </ul>
+    </section>
+  )
+}
---
+++ src/app/components/ContactMe.jsx
@@ -0,0 +1,17 @@
+// About me section
+
+export default function ContactMe() {
+  return (
+    <section>
+      <h1>Contact Me</h1>
+
+        <button>My Email</button>
+        <p>
+            I am seeking internship opportunities for summer 2026, along with full-time positions, and am available
+            to start immediately. I am currently
+            located in Richmond, VA but am willing to relocate. I also have a flexible graduation timeline and can
+            graduate early, enabling me to finish my degree while working full-time if needed.
+        </p>
+    </section>
+  )
+}
